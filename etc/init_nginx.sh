39c430fa7ef327a48dc861ffd0fa3a3fc49afb9e
MyWayToJavaJunior <hyperjava@mail.ru>
2016-03-02 15:59:59 +0600

    update

--- etc/init_nginx.sh
+++ etc/init_nginx.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 /etc/init.d/nginx start
-=======
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
->>>>>>> e533466daa87d6603e15daec8a8495b60ba82435
